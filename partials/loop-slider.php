bac0c443b1a1adc12ed0893bebf61e9d9e7a5708
alastair38 <alastair@alastaircox.com>
2015-03-24 17:27:31 +0000

    Removed slider wrapper which was preventing slide container from being focused and so interferring with keyboard navigation

--- partials/loop-slider.php
+++ partials/loop-slider.php
@@ -1,7 +1,4 @@
 
-                       	<div class="slider-wrapper">
-
-
                             <div class="slider" id="frontPagecontent">
                                 <div>
                                   <?php echo wp_kses (get_field('slide_one'), array('br' => array(),'p' => array())); ?>
@@ -10,9 +7,8 @@
                                    <?php echo wp_kses (get_field('slide_two'), array('br' => array(),'p' => array())); ?>
                                 </div>
                             </div>
-                                <div class="controls">
+
+                            <div class="controls">
                                 <button aria-hidden="true" class="next"></button>
                                 <button aria-hidden="true" class="prev"></button>
-
                             </div>
-                        </div>
